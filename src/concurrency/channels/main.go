2064bdd00986b81f5d39cbd797a884dd7b1feb86
Bryan Azofeifa <bazofeifa@beenverified.com>
2017-09-12 08:33:36 -0600

    Allow to switch between channels

--- src/concurrency/channels/main.go
+++ src/concurrency/channels/main.go
@@ -1,13 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	basicChannel()
+	//basicChannel()
+	//bufferedChannel()
+	closeChannel()
 }
 
 func basicChannel() {
 	ch := make(chan string, 1)
 	ch <- "Hello"
 	fmt.Println(<-ch)
+}
+
+func bufferedChannel() {
+	phrase := "These are the times that try men's souls.\n"
+
+	words := strings.Split(phrase, " ")
+
+	ch := make(chan string, len(words))
+
+	for _, word := range words {
+		ch <- word
+	}
+
+	for i := 0; i < len(words); i++ {
+		fmt.Print(<-ch + " ")
+	}
+}
+
+func closeChannel() {
+	phrase := "Closing channel message.\n"
+
+	words := strings.Split(phrase, " ")
+
+	ch := make(chan string, len(words))
+
+	for _, word := range words {
+		ch <- word
+	}
+
+	close(ch)
+
+	//for {
+	//	if msg, ok := <-ch; ok {
+	//		fmt.Print(msg + " ")
+	//	} else {
+	//		break
+	//	}
+	//}
+
+	// Simpler code
+	for msg := range ch {
+		fmt.Print(msg + " ")
+	}
 }
